788f47379166a915b924ecd2d5e53472ca6462e5
Hadrian Hughes <hadrianhughes@gmail.com>
2022-08-08 16:36:27 +0100

    Get diffs relative to their preceding commits

--- src/git.ts
+++ src/git.ts
@@ -46,13 +46,13 @@ const displayDiff = async () => {
 }
 
 export const runDiff = async (commit: string) => {
-  const diffFilesResult = await run(`git diff --name-only ${commit}`, 'error getting files to diff')
+  const diffFilesResult = await run(`git diff --name-only ${commit}^ ${commit}`, 'error getting files to diff')
   const diffFiles = diffFilesResult.split('\n')
 
   const diffs = []
 
   for (const f of diffFiles) {
-    const rawDiff = await run(`git diff --color=always ${commit} -- ${f}`, `error diffing file ${f} at commit ${commit}`)
+    const rawDiff = await run(`git diff --color=always ${commit}^ ${commit} -- ${f}`, `error diffing file ${f} at commit ${commit}`)
     const formattedDiff = formatDiff(rawDiff)
     diffs.push(formattedDiff)
   }
@@ -64,7 +64,7 @@ export const runDiff = async (commit: string) => {
 }
 
 export const runDiffForFile = async (commit: string, file: string) => {
-  const diffResult = await run(`git diff --color=always ${commit} -- ${file}`, `error diffing file ${file} at commit ${commit}`)
+  const diffResult = await run(`git diff --color=always ${commit}^ ${commit} -- ${file}`, `error diffing file ${file} at commit ${commit}`)
   const formattedDiff = formatDiff(diffResult)
 
   await saveDiff(formattedDiff)
